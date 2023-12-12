ec483153db94197ed5c9a35114ab427e283c3a7b
Natan <zerogamerptbr@gmail.com>
2023-12-12 13:34:30 -0300

    :sparkles:feat:transaction entity

---
+++ src/main/java/com/PicPayTest/domain/transaction/Transaction.java
@@ -0,0 +1,37 @@
+package com.PicPayTest.domain.transaction;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import com.PicPayTest.domain.user.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity(name = "transactions")
+@Table()
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private BigDecimal amount;
+    @ManyToOne
+    @JoinColumn(name = "senderID")
+    private User sender;
+    @ManyToOne
+    @JoinColumn(name = "receiverID")
+    private User receiver;
+    private LocalDateTime timeStamp;
+}
