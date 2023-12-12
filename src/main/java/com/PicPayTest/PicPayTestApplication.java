269e19d3639977bcb5169fd0a3aaa36993910460
Natan <zerogamerptbr@gmail.com>
2023-12-12 09:54:10 -0300

    :tada:initial commit

---
+++ src/main/java/com/PicPayTest/PicPayTestApplication.java
@@ -0,0 +1,14 @@
+package com.PicPayTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicPayTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicPayTestApplication.class, args);
+		System.out.println("Server is running");
+	}
+
+}
