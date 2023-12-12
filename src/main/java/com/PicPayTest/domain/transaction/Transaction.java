a5d5a36a4f6f91b30b277c2b5235cf54b2d2a5b9
Natan <zerogamerptbr@gmail.com>
2023-12-12 14:57:33 -0300

    :sparkles:feat: Transaction entity

--- src/main/java/com/PicPayTest/domain/transaction/Transaction.java
+++ src/main/java/com/PicPayTest/domain/transaction/Transaction.java
@@ -14,6 +14,7 @@ import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity(name = "transactions")
@@ -21,6 +22,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode(of = "id")
 public class Transaction {
     @Id
