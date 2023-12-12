c2b07d52a20f7e288cd208cdb33fb77caeb7fdb9
Natan <zerogamerptbr@gmail.com>
2023-12-12 20:03:42 -0300

    :recycle:refac:user entity to improve development

--- src/main/java/com/PicPayTest/domain/user/User.java
+++ src/main/java/com/PicPayTest/domain/user/User.java
@@ -4,6 +4,8 @@ import jakarta.persistence.GenerationType;
 
 import java.math.BigDecimal;
 
+import com.PicPayTest.dtos.UserDTO;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -14,6 +16,7 @@ import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 
@@ -22,6 +25,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode(of="id")
 public class User {
     @Id
@@ -37,4 +41,13 @@ public class User {
     private BigDecimal balance;
     @Enumerated(EnumType.STRING)
     private UserType userType;
+
+    public User(UserDTO data){
+        this.firstName = data.firstName();
+        this.lastName = data.lastName();
+        this.balance = data.balance();
+        this.userType = data.userType();
+        this.password = data.password();
+        this.email = data.email();
+    }
 }
